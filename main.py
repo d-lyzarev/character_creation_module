f141190a5034ba947e029faac957133ec84afa9d
Дмитрий Лазарев <lyzarev@yandex.ru>
2023-04-22 15:53:05 +0300

    removal to laptop

--- main.py
+++ main.py
@@ -2,10 +2,7 @@ from random import randint
 
 
 def attack(char_name: str, char_class: str) -> str:
-<<<<<<< HEAD
     """Атака."""
-=======
->>>>>>> bfe523490677976cd0ec70fc06b8e335e5363efb
     if char_class == 'warrior':
         return (
             f'{char_name} нанёс урон противнику'
@@ -20,10 +17,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
-<<<<<<< HEAD
     """Защита."""
-=======
->>>>>>> bfe523490677976cd0ec70fc06b8e335e5363efb
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -33,10 +27,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
-<<<<<<< HEAD
     """Специальное умение."""
-=======
->>>>>>> bfe523490677976cd0ec70fc06b8e335e5363efb
     if char_class == 'warrior':
         return (
             f'{char_name} применил специальное умение'
@@ -48,10 +39,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
-<<<<<<< HEAD
     """Начало тренировки."""
-=======
->>>>>>> bfe523490677976cd0ec70fc06b8e335e5363efb
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -77,10 +65,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
-<<<<<<< HEAD
     """Выбор персонажа."""
-=======
->>>>>>> bfe523490677976cd0ec70fc06b8e335e5363efb
     approve_choice: str = ('')
     char_class: str = ('')
     while approve_choice != 'y':
@@ -115,8 +100,4 @@ def main():
     print(start_training(char_name, char_class))
 
 
-<<<<<<< HEAD
-main() 
-=======
 main()
->>>>>>> bfe523490677976cd0ec70fc06b8e335e5363efb
